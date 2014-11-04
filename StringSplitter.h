9e3fc15d6b6d9bf86d028d55f0464500eae16b21
Dillon Godwin <dillon.godwin@email.wsu.edu>
2014-11-04 12:21:47 -0800

    Update StringSplitter.h
    Typed this file by hand the first time and had tons of errors. Copy and past now of Adam's original code. Works perfectly

--- StringSplitter.h
+++ StringSplitter.h
@@ -7,39 +7,45 @@
 #include <string>
 #include <vector>
 
-using namepsace std;
+using namespace std;
 
 class StringSplitter
 {
 public:
-	//Breaks apart the supplied text based ont he given delimiter
-	vector<string> pieces;
 
-	//find the first delimiter
-	int location = text.find(delimiter);
-	
-	//we are starting at the beginning of our string
-	int start = 0;
-
-	//go until we have no more delimiters
-	while (location != string::npos)
+	//Breaks apart the supplied text based on the given delimiter
+	//static function do not affect the internal state
+	//(e.g. variables) of a given class instance
+	static vector<string> split(string text, string delimiter)
 	{
-		//add the current piece to our list of pieces
-		string piece = text.substr(start, location - start_;
+		//vectors are dynamically expanding arrays
+		vector<string> pieces;
+
+		//find the first delimiter
+		int location = text.find(delimiter);
+
+		//we are starting at the beginning of our string
+		int start = 0;
+
+		//go until we have no more delimiters
+		while (location != string::npos)
+		{
+			//add the current piece to our list of pieces
+			string piece = text.substr(start, location - start);
+			pieces.push_back(piece);
+
+			//update our index markers for the next round
+			start = location + 1;
+			location = text.find(delimiter, start);
+		}
+
+		//at the end of our loop, we're going to have one trailing piece to take care of.
+		//handle that now.
+		string piece = text.substr(start, location - start);
 		pieces.push_back(piece);
 
-		//update our index markers for the next round
-		start = location + 1;
-		location = text.find(delimiter, start);
-	}
-
-	//at the end of our loop, we're going to have one trailing piece to take care of
-	//handle that now.
-	string piece = text.substr(start, location - start);
-	pieces.push_back(piece);
-
-	//now, return the completed vector
-	return pieces;
+		//now, return the completed vector
+		return pieces;
 	}
 };
 
